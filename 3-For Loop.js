a221ce35e56a0ea253117a3222ad6117426db45d
sthrasher17 <32141005+sthrasher17@users.noreply.github.com>
2017-10-17 13:08:19 -0400

    Update 3-For Loop.js

--- 3-For Loop.js
+++ 3-For Loop.js
@@ -1,29 +1,36 @@
 /* 3-For Loop */
 
-//conditional where flavor = chocolate or vanilla
-//prompt for flavor, then output "heres your flavor"
-//if strawberry alert "all out of "strawberry"
-//Else alert "no such flavor"
-//Put in a for loop asking for 3 scoops
-//After Loop, alert "Here's your"+flavor, flavor, flavor+"cone".
+// flavors are chocolate, vanilla, strawberry
+// we are out of strawberry
+// create a for loop for three flavors
+// allow selecting another flavor if strawberry or not a flavor
+// alert "Here's your" + flavor when a good flavor is picked
 
-
-
-
-for(i = 0; i < 0; i++){
-	flavor = prompt("Choose the flavor of ice cream that you want.");
-
-if (flavor == "chocolate" || flavor == "vanilla"){
-	alert("Here's your "+flavor+" ice cream");
-	i--;
+for (scoop=1;scoop<4;scoop++) {
+	flavor = prompt("What flavor scoop?");
+	if (flavor=="chocolate" || flavor=="vanilla"){
+		alert("Here's your "+flavor+" scoop");
+	}
+	else if (flavor == "strawberry") {
+		alert("All out of "+flavor);
+		scoop=scoop-1;
+		}
+	else {
+		alert(flavor+" is not a flavor.");
+		scoop=scoop-1;
+		}
 }
-else if (flavor == "strawberry"){
-	alert("Sorry we are all out of strawberry.");
-	i--;
-}
-else{
-	alert("There is no such flavor.");
-	i--;
-}
-alert("Scoop"+1+");
+
+
+
+
+
+function inArray(svalue, tarray){
+	answer=false;
+	for (index=0;index<tarray.length;index++){
+		if (svalue==tarray[index]){
+			answer=true;
+		}
+	}
+	return answer;
 }
